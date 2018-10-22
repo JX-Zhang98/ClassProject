ba4e6693189109d59b7f069f6d0da539271bba94
f0110vv3r <1773262526@qq.com>
2018-10-22 23:35:31 +0800

    start to work for stockSys

---
+++ 软件体系结构/StockSystem/user.cpp
@@ -0,0 +1,15 @@
+#include "implementation.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+int EXAMPLE_MAIN (int argc, char *argv[])
+{
+
+
+
+
+
+	examples::dcps::ContentFilteredTopic::isocpp::subscriber(argc, argv);
+}

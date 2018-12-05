060e9304624b9d09ae5a2cbe99f08d956d48aec8
e <f0110vv3r@gmail.com>
2018-12-05 23:54:04 +0800

    finishi to connect with sql,try to jump to another page

---
+++ 数据库-信息安全竞赛系统/ISsys/reg.pas
@@ -0,0 +1,28 @@
+unit reg;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  Tregist = class(TForm)
+    Button1: TButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  regist: Tregist;
+
+implementation
+
+{$R *.dfm}
+
+
+
+
+end.
